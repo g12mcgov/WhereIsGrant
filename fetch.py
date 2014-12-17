338e84930876efecde96b7d32356c117545715a4
Grant McGovern <grant@cobrain.com>
2014-12-17 00:27:47 +0000

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -10,12 +10,12 @@ class Twitter():
 
 	def login(self):
 		## Credentials 
-		consumer_key = 'lZqAC2G5ReXqFD1G7VYqumbCv'
-		consumer_secret = 'slijdIL4mMtz09Ism7eCurqfHbxJ8RaodvG6GbOhKlmdg2RWas'
+		consumer_key = 'D1G7VYqumbCv'
+		consumer_secret = 'slijdIL4urqfHbxJ8RaodvG6GbOhKlmdg2RWas'
 
 		## Access Tokens 
-		access_token = '45555007-dJKIsb3Lqt3h12XvZcPgDoG9dtzyy93Uq7djsG2Wq'
-		access_token_secret = 'UH6VVIF56ei6y5ktAmkIw0wF7wlDUYEwwTXrfZksPpbvP'
+		access_token = '45555007-dJKIsb3cPgDoG9dtzyy93Uq7djsG2Wq'
+		access_token_secret = 'UH6VVIF56ei6y5ktTXrfZksPpbvP'
 
 		## Authenticate App
 		auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
